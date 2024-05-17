a70b669334c512df2c77756428efeb7045bb866c
krishna sharma <134923185+coderkrishna2441@users.noreply.github.com>
2024-05-17 15:33:32 +0530

    Create 26.js

---
+++ T-1/26.js
@@ -0,0 +1,10 @@
+//Write a JSON script to store information related to books based on their id, topic,edition and author. 
+// (Minimum details of three books having id=1,2,3) 
+
+var books = [
+    {"id":1,"topic":"Science","edition":5,"author":"XYZ"},
+    {"id":2,"topic":"Maths","edition":2,"author":"PQR"},
+    {"id":3,"topic":"Python","edition":7,"author":"MNO"}
+];
+
+console.log(books);
