63b00323b4ee44ea327ddaf9e17027882fb69911
krishna sharma <134923185+coderkrishna2441@users.noreply.github.com>
2024-05-19 17:11:43 +0530

    Create 30.js

---
+++ T-1/30.js
@@ -0,0 +1,14 @@
+// Write a script to define two JSON objects named as “division1” and “division2” 
+// having an array to store 5 names of students along with their roll number and list 
+// of subjects they opted. 
+
+var Division1 = {
+    "name":["Krishna","Vrunda","Rumesha","Tithi","Khushi"],
+    "rollno":["86","87","84","104","67"],
+    "subject":[["Python","Sql","TOC"],["FSD","Sql","Java"],["Java","FSD","DM"],["Python","DM","TOC"],["TOC","COA","DM"]]
+};
+var Division2 = {
+    "name":["karthik","Divya","Aaksh","Nilay","Smit"],
+    "rollno":["68","78","48","14","76"],
+    "subject":[["Python","Sql","TOC"],["FSD","Sql","Java"],["Java","FSD","DM"],["Python","DM","TOC"],["TOC","COA","DM"]]
+};
